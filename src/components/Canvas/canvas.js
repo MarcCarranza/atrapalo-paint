dd66a33218c1318b772cd1822c26c0bf449f2315
Marc <a15marcarfer@iam.cat>
2018-12-03 14:14:07 +0100

    Modificado canvas con dibujo básico y responsive

---
+++ src/components/Canvas/canvas.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Stage, Layer, Text, Line } from 'react-konva';
+import classes from './canvas.module.css';
+
+class Canvas extends Component {
+  state = {
+    lines: [],
+    width: window.innerWidth * .7,
+    height: window.innerHeight * .8
+  };
+  
+  handleMouseDown = () => {
+    this._drawing = true;
+    this.setState({
+      lines: [...this.state.lines, []]
+    });
+  };
+
+  handleMouseMove = e => {
+    if (!this._drawing) {
+      return;
+    }
+    const stage = this.stageRef.getStage();
+    const point = stage.getPointerPosition();
+    const { lines } = this.state;
+
+    let lastLine = lines[lines.length - 1];
+    lastLine = lastLine.concat([point.x, point.y]);
+
+    lines.splice(lines.length - 1, 1, lastLine);
+    this.setState({
+      lines: lines.concat()
+    });
+  };
+
+  handleMouseUp = () => {
+    this._drawing = false;
+  };
+
+  render() {
+    return (
+      <div className={classes.Canvas}>
+        <Stage
+          width={this.state.width}
+          height={this.state.height}
+          onContentMousedown={this.handleMouseDown}
+          onContentMousemove={this.handleMouseMove}
+          onContentMouseup={this.handleMouseUp}
+          ref={node => {
+            this.stageRef = node;
+          }}
+        >
+          <Layer>
+            <Text text="Just start drawing" />
+            {this.state.lines.map((line, i) => (
+              <Line key={i} points={line} stroke="red" />
+            ))}
+          </Layer>
+        </Stage>
+      </div>
+    );
+  }
+}
+
+export default Canvas;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import classes from './App.css';
+import classes from './App.module.css';
+import Canvas from '../components/Canvas/canvas';
 
 class App extends Component {
   render() {
     return (
       <div className = {classes.App}>
-        
+        <Canvas className={classes.Canvas}></Canvas>
       </div>
     );
   }

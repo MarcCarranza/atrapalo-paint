cdc42d7ed993259a8dcf02296a45c82462740fcf
Carranza <mcarranza@contractor.deloitte.es>
2018-12-10 13:22:37 +0100

    Añadido enzyme y test de prueba
    Signed-off-by: Carranza <mcarranza@contractor.deloitte.es>

--- src/components/__tests__/canvas.test.jsx
+++ src/components/__tests__/canvas.test.jsx
@@ -1,10 +1,20 @@
 import { shallow } from 'enzyme';
 import Canvas from '../Canvas/canvas';
 
-describe('BaseButton', () => {
-    it('renders correctly', () => {
-      const wrapper = shallow(<Canvas />);
-      expect(wrapper).toMatchSnapshot();
-      // On the first run of this test, Jest will generate a snapshot file automatically.
+describe('Canvas', () => {
+  describe('when user clicks button', () => {
+    it ('calls correct function to save the information', () => {
+      const onButtonClickMock = jest.fn();
+      const wrapper = shallow(
+        <Canvas
+          onButtonClick={onButtonClickMock}
+        />,
+      );
+      const buttonElement = wrapper.find(Canvas); // step 1 above
+      buttonElement.simulate('click'); // step 2
+      
+      expect(onButtonClickMock).toHaveBeenCalledTimes(1); // step 3
+      expect(onButtonClickMock).toHaveBeenCalledWith(true);
     });
   });
+});

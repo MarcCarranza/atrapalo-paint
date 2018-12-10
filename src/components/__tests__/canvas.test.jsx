4acea91f77b105cedf8902ddbdbcd9fa828080e1
Carranza <mcarranza@contractor.deloitte.es>
2018-12-10 14:56:52 +0100

    Añadido mock al hacer click en canvas
    Signed-off-by: Carranza <mcarranza@contractor.deloitte.es>

--- src/components/__tests__/canvas.test.jsx
+++ src/components/__tests__/canvas.test.jsx
@@ -5,9 +5,15 @@ import Canvas from "../Canvas/canvas";
 
 describe("Canvas", () => {
   Enzyme.configure({ adapter: new Adapter()})
+  const clickTest = jest.fn();
   it("renders correctly", () => {
-    const wrapper = Enzyme.shallow(<Canvas />);
+    const wrapper = Enzyme.shallow(
+      <Canvas 
+        click={clickTest}
+        />
+    );
     expect(wrapper).toMatchSnapshot();
+    expect(clickTest).toHaveBeenCalled();
     // On the first run of this test, Jest will generate a snapshot file automatically.
   });
 });
